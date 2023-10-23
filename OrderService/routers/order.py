48eeec61394b592a9acd7977b9ebf2e12350aaac
AleksKhrap <112804992+AleksKhrap@users.noreply.github.com>
2023-10-23 21:10:09 +0300

    Add GET method

--- OrderService/routers/order.py
+++ OrderService/routers/order.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends  # , HTTPException
 from typing import List
 from sqlalchemy.orm import Session
 from OrderService.models import models, schemas
-# from OrderService.controllers.order import get_order
+from OrderService.controllers.order import get_orders
 from OrderService.models.database import engine
 from OrderService.models.database import SessionLocal
 
@@ -20,18 +20,16 @@ def get_db():
         db.close()
 
 
-'''
-@app.get('/', response_model=List[schemas.Product])
-async def read_products(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    products = get_products(db, skip=skip, limit=limit)
-    return products
+@app.get('/', response_model=List[schemas.Order])
+async def read_orders(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    orders = get_orders(db, skip=skip, limit=limit)
+    return orders
 
 
 @app.post('/')
-async def create_product(product: schemas.Product, db: Session = Depends(get_db)):
-    db_product = models.Products(**product.dict())
-    db.add(db_product)
+async def create_order(order: schemas.Order, db: Session = Depends(get_db)):
+    db_order = models.Order(**order.model_dump())
+    db.add(db_order)
     db.commit()
-    db.refresh(db_product)
-    return db_product
-    '''
+    db.refresh(db_order)
+    return db_order
