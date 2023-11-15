88be2ca0434a6f1073e9321ff9ce3c9fd8ce5043
AleksKhrap <112804992+AleksKhrap@users.noreply.github.com>
2023-11-15 11:52:41 +0300

    Add UUID to field product_id

--- microservices/ProductService/product_app/models/models.py
+++ microservices/ProductService/product_app/models/models.py
@@ -1,11 +1,13 @@
 from sqlalchemy import Column, String, Float
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
 from ProductService.product_app.db.database import Base
 
 
 class Products(Base):
     __tablename__ = "products"
 
-    product_id = Column(String, primary_key=True)
+    product_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, index=True)
     name = Column(String)
     weight = Column(Float)
     description = Column(String)
