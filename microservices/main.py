13db43bf21a126d0fa5bb508f4822210c72c58b5
AleksKhrap <112804992+AleksKhrap@users.noreply.github.com>
2023-12-17 18:20:07 +0300

    Create GraphQL service to combine Product and Order services

--- microservices/main.py
+++ microservices/main.py
@@ -5,6 +5,7 @@ from ProductService.product_app.routers.products import router as product_router
 from OrderService.order_app.routers.order import router as order_router
 from InventoryService.inventory_app.routers.inventory import router as inventory_router
 from NotificationService.notific_app.routers.notification import router as notification_router
+from Graphql.graph import graphql_app
 
 tags_metadata = [
     {
@@ -22,10 +23,18 @@ tags_metadata = [
     {
         "name": "NotificationService",
         "description": "Read all notifications.",
+    },
+    {
+        "name": "GraphQL",
+        "description": "ProductService with OrderService"
     }
 ]
 
-app = FastAPI(openapi_tags=tags_metadata)
+app = FastAPI(openapi_tags=tags_metadata,
+              title="Microservices",
+              description="Some FastAPI services. For more details about GraphQL, "
+                          "visit [GraphQL Service](http://127.0.0.1:8000/graphql)"
+              )
 
 
 @app.exception_handler(RequestValidationError)
@@ -49,3 +58,4 @@ app.include_router(product_router, prefix='/product', tags=["ProductService"])
 app.include_router(order_router, prefix='/order', tags=["OrderService"])
 app.include_router(inventory_router, prefix='/inventory', tags=["InventoryService"])
 app.include_router(notification_router, prefix="/notification", tags=["NotificationService"])
+app.include_router(graphql_app, prefix='/graphql', tags=["GraphQL"], include_in_schema=False)
